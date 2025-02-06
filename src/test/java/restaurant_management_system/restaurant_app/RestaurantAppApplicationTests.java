6c1e8ebc588d6f61f6ae719f703833b502e8c61d
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2025-02-06 19:15:17 +0700

    First commit

---
+++ src/test/java/restaurant_management_system/restaurant_app/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurant_management_system.restaurant_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
